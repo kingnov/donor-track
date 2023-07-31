a4c4aa644339ec957cb5ee35eabb8ce9ad7fa57e
Neala Achieng <achneala49@gmail.com>
2023-07-31 16:04:52 +0300

    did functionalities

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,47 +1,91 @@
 import Google from "../assets/images/google.png";
+import { useState } from "react";
 
 
 function SignUp() {
+    const [show, setShow] = useState([false]);
+    const [formData, setFormData] = useState({});
+    const [error, setError] = useState({});
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+    function handleSignUp(e) {
+        e.preventDefault();
+        const errors = {};
+        (formData.firstName === "" || formData.firstName === undefined) &&
+          (errors.firstName = "Please enter your first name");
+        (formData.lastName === "" || formData.lastName === undefined) &&
+          (errors.lastName = "Please enter your last name");
+        (formData.emailAddress === "" || formData.emailAddress === undefined) &&
+          (errors.emailAddress = "please enter your emailAddress");
+        (formData.phoneNumber === "" || formData.phoneNumber === undefined) &&
+          (errors.phoneNumber = "please enter your phoneNumber");
+        (formData.passWord === "" || formData.passWord === undefined) &&
+          (errors.passWord = "please enter your passWord");
+    
+        setError(errors);
+    
+        console.log(errors);
+      }
+    
+    
+
+
   return (    <div className="bg-[ linear-gradient(rgba(0,0,0,.5),rgba(0,0,0,.5)),url('assets/images/upperHero.jpg')]  bg-no-repeat bg-cover h-[80vh]">
       <div className="w-[50%] text-center p-5 mx-auto ">
         <h1 className="text-4xl font-bold my-8 ">Create An Account</h1>
         <form action="" className="flex flex-col">
           <div className="flex justify-between my-8">
             <div>
-
+            {error.firstName && (
+                <p className="text-red-500">{error.firstName}</p>
+              )}
               <input
                 type="text"
                 placeholder="First Name"
                 className="rounded-full py-4 px-6 border-2 outline-none border-gray-300 w-[50%]"
+                onChange={(e) => handleChange(e)}
                 name="firstName"
               />
             </div>
             <div>
+            {error.firstName && (
+                <p className="text-red-500">{error.firstName}</p>
+              )}
               <input
                 type="text"
                 placeholder="Last Name"
                 className="rounded-full py-4 px-6 border-2 outline-none border-gray-300 w-[50%]"
-                
+                onChange={(e) => handleChange(e)}
                 name="lastName"
               />
             </div>
           </div>
           <div className="flex flex-col gap-8">
-
+          {error.firstName && (
+                <p className="text-red-500">{error.firstName}</p>
+              )}
             <input
               type="email"
               placeholder="Email Address"
               className="rounded-full py-4 px-6 border-2 outline-none border-gray-300 w-[100%]"
-            
+              onChange={(e) => handleChange(e)}
               name="emailAddress"
             />
+             {error.firstName && (
+                <p className="text-red-500">{error.firstName}</p>
+              )}
             <input
               type="text"
               placeholder="Phone Number"
               className="rounded-full py-4 px-6 border-2 outline-none border-gray-300 w-[100%]"
+              onChange={(e) => handleChange(e)}
               name="phoneNumber"
             />
             <div className="flex border-2 items-center rounded-full py-4 px-6 ">
+            {error.firstName && (
+                <p className="text-red-500">{error.firstName}</p>
+              )}
               <input
                 type="pas"
                 placeholder="Password"
@@ -50,10 +94,14 @@ function SignUp() {
               />
             </div>
             <div className="flex border-2 items-center rounded-full py-4 px-6 ">
+            {error.firstName && (
+                <p className="text-red-500">{error.firstName}</p>
+              )}
               <input
                 type="password"
                 placeholder="Confirm your password"
                 className=" outline-none w-[100%]"
+                onChange={(e) => handleChange(e)}
               />
             </div>
           </div>
@@ -73,6 +121,7 @@ function SignUp() {
           <div>
             <button className="py-4 px-2 my-5 border-2 border-grey-400 gap-4 flex items-center rounded-full w-{70%} text-3xl text-sky-600 font-bold">
               <img src={Google} alt="" width={50} />
+              onChange={(e) => handleChange(e)}
               <div className="text-3xl text-blue-500 font-bold" >
                 <p>Sign In With Google</p>
               </div>
