30fed9cd340c095bbcea9c27f4b4daef8de1a6b3
Neala Achieng <achneala49@gmail.com>
2023-08-02 14:47:43 +0300

    did authentication

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,35 +1,58 @@
 import Google from "../assets/images/google.png";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from "../firebase";
 
 function LogIn() {
-    const [logData, setLogData] = useState({});
-    const [error, setError] = useState({});
-    const handleChange = (e) => {
-      setLogData({ ...logData, [e.target.name]: e.target.value });
-    };
-    function handleLogIn(e) {
-        e.preventDefault();
-        const errors = {};
-        (logData.emailAddress === "" || logData.emailAddress === undefined) &&
-          (errors.emailAddress = "please enter your emailAddress");
-        (logData.passWord === "" || logData.passWord === undefined) &&
-          (errors.passWord = "please enter your passWord");
-    
-        setError(errors);
-        console.log(errors);
-      }
-  
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+  const auth = getAuth(app);
+  const [logData, setLogData] = useState({});
+  const [error, setError] = useState({});
+  const [loginError, setLoginError] = useState("");
+  const emailRef = useRef();
+  const handleChange = (e) => {
+    setLogData({ ...logData, [e.target.name]: e.target.value });
+  };
+  function handleLogIn(e) {
+    e.preventDefault();
+    const errors = {};
+    (logData.emailAddress === "" || logData.emailAddress === undefined) &&
+      (errors.emailAddress = "please enter your emailAddress");
+    (logData.passWord === "" || logData.passWord === undefined) &&
+      (errors.passWord = "please enter your passWord");
 
+    setError(errors);
+    console.log(errors);
 
+    signInWithEmailAndPassword(auth, logData.emailAddress, logData.passWord)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user.email);
+        setLogData({
+          emailAddress: "",
+          passWord: "",
+        });
+        setLoginError("");
+        emailRef.current.focus();
+      })
+      .catch((error) => {
+        const errorMsg = error.message.substring(22, error.message.length - 2);
+        setLoginError(errorMsg);
+      });
+  }
   return (
     <div className="bg-[linear-gradient(rgba(0,0,0,.5),rgba(0,0,0,.5)),url('assets/images/upperHero.jpg')] bg-no-repeat  bg-cover h-[80vh]">
-      
       <div className="w-[40%] text-center mx-auto  ">
-        <h1 className="text-4xl font-bold my-8">Log in to your account</h1>
+        <h1 className="text-4xl font-bold my-8 pt-5 text-white">
+          Log in to your account
+        </h1>
+        {loginError !== "" && <p className="text-red-500">{loginError}</p>}
         <form action="" className="flex flex-col">
           <div className="flex flex-col gap-8">
-          {error.emailAddress && (
-              <p className="text-red-500">{error.emailAddress}</p>
+            {error.emailAddress && (
+              <p className="text-red-500 capitalize">{error.emailAddress}</p>
             )}
             <input
               name="emailAddress"
@@ -37,20 +60,21 @@ function LogIn() {
               type="email"
               placeholder="EmailAddress"
               className="rounded-full py-4 px-6 border-2 border-gray-300"
+              ref={emailRef}
+              value={logData.emailAddress}
             />
-        
-            {error.passWord && (
-              <p className="text-red-500">{error.passWord}</p>
-            )}
+
+            {error.passWord && <p className="text-red-500">{error.passWord}</p>}
             <div className="flex border-2 items-center rounded-full py-4 px-6 bg-white ">
-            <input
-              name="passWord"
-              onChange={(e) => handleChange(e)}
-              type="password"
-              placeholder="Password"
-              className="outline-none w-[100%] "
-            />
-             </div>
+              <input
+                name="passWord"
+                onChange={(e) => handleChange(e)}
+                type="password"
+                placeholder="Password"
+                className="outline-none w-[100%] "
+                value={logData.passWord}
+              />
+            </div>
           </div>
           <div className="flex my-8 justify-between">
             <div className="flex gap-4">
@@ -68,19 +92,7 @@ function LogIn() {
             Sign in
           </button>
         </form>
-        {/* <div className="flex items-center my[1em]">
-          <div className="flex-grow h-px bg-slate-400"> </div>
-          <p className="my-6">OR</p>
-          <div className="flex-grow h-px bg-slate-400"> </div>
-        </div> */}
-        {/* <div className="flex items-center gap-8 my-8 justify-centern border-2 border-grey-400 rounded-full bg-white">
-          <div>
-            <img src={Google} alt="" width={50} />
-          </div>
-          <div className="text-3xl text-blue-500 font-bold">
-            <p>Log In With Google</p>
-          </div>
-        </div> */}
+
         <p className="my-6 text-white">
           Don't have an account yet?{" "}
           <span className="text-[#5eebc1] ">Sign Up free</span>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ import logo from "../assets/images/logo1.png"
 function Nav() {
   return (
     <nav className="flex justify-between capitalize py-2 bg- container mx-auto items-center">
-      <h1> <img className="w-[80px]" src={logo} /></h1>
+      <h1> <img className="w-[100px]" src={logo} /></h1>
       <div>
         <ul className="flex gap-8 justify-between items-center p-2">
           <li><a href="/">Home</a></li>
